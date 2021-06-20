f4465b693e23c892eba44e8c5bc372f7f26ee522
Shafiq-Kyazze <shafiqkyazze@live.com>
2021-06-20 23:35:15 +0100

    Adding config file with Database URI

---
+++ config.py
@@ -0,0 +1,6 @@
+####  config.py  ####
+
+
+#Database where data is going to be uploaded and stored
+ELEPHANT_SQL_URI = "postgresql://**************************.db.elephantsql.com/firztpnr"
+Database_URI = ELEPHANT_SQL_URI
