0813d325cc940e089e3f4cc5e7be7f208a577044
Shafiq-Kyazze <58377262+Shafiq-Kyazze@users.noreply.github.com>
2021-06-21 19:32:09 +0100

    Adding more comments to the database module

--- database.py
+++ database.py
@@ -13,44 +13,46 @@ import shutil
 
 
 
-#Initialising engine
+#Connecting to database
 engine = create_engine(Database_URI, echo=False)
 
-#Binding session to engine
+#Binding session to engine i.e specific database
 Session= sessionmaker(bind=engine)
-s = Session()
+s = Session() # Allows python to interact with PostgreSQL
 
 
 
-#Creating table after import original base from models.py
+#Creating table after importing original Base from models module
 Base.metadata.create_all(bind=engine)
-s.commit()    #Committing changes
+s.commit()    #uploading newly created table
 
 
 
-
-sample_data_path = "C:/Users/shafi/Dropbox/Data Science Projects/GAZPROM/GazProm-Data-Engineering-Project/sample_data/"
-uploaded_data_path= "C:/Users/shafi/Dropbox/Data Science Projects/GAZPROM/GazProm-Data-Engineering-Project/Uploaded data/"
-Bad_data_path= "C:/Users/shafi/Dropbox/Data Science Projects/GAZPROM/GazProm-Data-Engineering-Project/Bad data/"
-files = os.listdir(sample_data_path)
+#File paths showing where the different files are
+Original_data_folder = "C:/Users/shafi/Dropbox/Data Science Projects/GAZPROM/GazProm-Data-Engineering-Project/sample_data/"
+uploaded_data_folder= "C:/Users/shafi/Dropbox/Data Science Projects/GAZPROM/GazProm-Data-Engineering-Project/Uploaded data/"
+Bad_data_folder= "C:/Users/shafi/Dropbox/Data Science Projects/GAZPROM/GazProm-Data-Engineering-Project/Bad data/"
+files = os.listdir(Original_data_folder)
 
 #Function that converts the time to correct format
 def time_convertor(raw_time):
-    length = len(str(int(raw_time)))
-    if length < 4:
+    length = len(str(int(raw_time)))  #length of time string
+    if length < 4:  #if the length is lessthan 4 i.e not not in a HH:MM format
         missing_values = 4 - length    #amount of values missing
         raw_time = '0'*missing_values + str(int(raw_time))   #missing values added at the front to provide the data in an accurate HH:MM format
-        Time = datetime.strptime( raw_time, "%H%M").time()
+        Time = datetime.strptime( raw_time, "%H%M").time()   #Time converison
         return Time
     else:
-        raw_time = str(int(raw_time))
-        Time = datetime.strptime( raw_time, "%H%M").time()
+        raw_time = str(int(raw_time))  #Changing time to an integer to get rid of the digits after the point
+        Time = datetime.strptime( raw_time, "%H%M").time() #Time converison
         return Time
 
 for file in files:
-    df = pd.read_csv(sample_data_path+file)
+    df = pd.read_csv(Original_data_folder+file) #Pandas dataframe for a given file
     To_Head = list(df.columns) #List of columns in dataframe
-    N_rows = len(df.index)-1 #Number of rows in dataframe excluding the footer
+    N_rows = len(df.index) - 1 #Number of rows in dataframe excluding the footer
+
+
     #if function that checks if whole file was tranferred i.e headers and footers
     if (To_Head[0] == 'HEADR') & (df.iloc[-1][0] == 'TRAIL'):
         df.drop(df.columns[-1], axis=1, inplace=True) #Dropping File_Generation_Number column since its the last column
@@ -82,30 +84,34 @@ for file in files:
 
                 #if function that checks if query returned isn't empty i.e the data is going to be overwritten
                 if over_write_query is not None:
-                    over_write_result = s.query(CONSU).filter(CONSU.Meter_Number == float(row[1]), CONSU.Measurement_Date == row[2], CONSU.Measurement_Time ==row[3])\
-                        .update({CONSU.Record_Identifier:row[0], CONSU.Meter_Number: float(row[1]), CONSU.Measurement_Date: row[2], CONSU.Measurement_Time:row[3],
-                                 CONSU.Consumption:float(row[4]), CONSU.Header_id:headers.id}, synchronize_session="evaluate")
+                    #Query with matching parameters is overwritten
+                    over_write_result = s.query(CONSU).filter(CONSU.Meter_Number == float(row[1]), CONSU.Measurement_Date == row[2],
+                                                              CONSU.Measurement_Time ==row[3]).update({CONSU.Record_Identifier:row[0],
+                                                               CONSU.Meter_Number: float(row[1]), CONSU.Measurement_Date: row[2],
+                                                                CONSU.Measurement_Time:row[3],CONSU.Consumption:float(row[4]),
+                                                                CONSU.Header_id:headers.id}, synchronize_session="evaluate")
+
                 else:
                     consus = CONSU(Record_Identifier=row[0], Meter_Number=float(row[1]), Measurement_Date=row[2],
                                    Measurement_Time=row[3], Consumption=float(row[4]), Header_id=headers.id)
-                    s.add(consus)
+                    s.add(consus) #Inserting rows in  table
 
             # #Moving file to new location to prevent duolication
-            Old_location = sample_data_path + file #Original data folder
-            New_location = uploaded_data_path + file #Target data folder
+            Old_location = Original_data_folder + file #Original data folder
+            New_location = uploaded_data_folder + file #Target data folder
             shutil.move(Old_location, New_location)
 
         except IntegrityError:  #Python returns Integrity error if the File Genreation Number is non-unique
             s.rollback()
             # Moving repeated file to uploaded data folder
-            Old_location = sample_data_path + file  # Original data folder
-            Target_location = Bad_data_path+file
+            Old_location = Original_data_folder + file  # Original data folder
+            Target_location = Bad_data_folder+file
             shutil.move(Old_location,Target_location)
 
     else:
         # Moving bad data to bad data folder
-        Old_location = sample_data_path + file  # Original data folder
-        Target_location = Bad_data_path + file
+        Old_location = Original_data_folder + file  # Original data folder
+        Target_location = Bad_data_folder + file  #Target location for bad data
         shutil.move(Old_location, Target_location)
 
 s.commit()
