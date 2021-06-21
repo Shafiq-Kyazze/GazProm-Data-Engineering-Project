5ea9182ffa0e4502987a81c7978806be6bb6b89f
Shafiq-Kyazze <58377262+Shafiq-Kyazze@users.noreply.github.com>
2021-06-21 17:43:44 +0100

    Switch Database from Elephant SQL to local PostgreSQL since its faster when executing command queries

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 
 
 #Database where data is going to be uploaded and stored
-ELEPHANT_SQL_URI = "postgresql://**************************.db.elephantsql.com/firztpnr"
-Database_URI = ELEPHANT_SQL_URI
+Postgres_URI = "postgresql://postgres:password@localhost/GAZPROM"
+Database_URI = Postgres_URI
