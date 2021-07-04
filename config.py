70caec74e925f84272436b6c85acfe2918ab91ae
Shafiq-Kyazze <58377262+Shafiq-Kyazze@users.noreply.github.com>
2021-07-04 23:25:12 +0100

    Initial Adding files to github

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 
 
 #Database where data is going to be uploaded and stored
-Postgres_URI = "postgresql://postgres:password@localhost/GAZPROM"
+Postgres_URI = "postgresql://postgres:november228@localhost/GAZPROM"
 Database_URI = Postgres_URI
--- views.py
+++ views.py
@@ -8,19 +8,21 @@ s= Session()
 
 
 
+
 #How many meters are in the dataset?
-meters = s.query(func.count(distinct(CONSU.Meter_Number))).all()
+# meters = s.query(func.count(distinct(CONSU.Meter_Number))).all()
+#
+# #What is all the data for a given meter i.e How many records are there for meter 5 in the dataset?
+# data_for_given_meter = s.query(func.count(CONSU.Meter_Number)).filter(CONSU.Meter_Number==5).all()
+#
+# #How many files have we received?
+# files_received = s.query(func.count(HEADR.id)).all()
+#
+# #What was the last file to be received
+# last_file_uploaded = s.query(HEADR).order_by( desc(HEADR.Upload_Datetime)).first()
+# last_file_uploaded.__dict__['File_Generation_Number']
 
-#What is all the data for a given meter i.e How many records are there for meter 5 in the dataset?
-data_for_given_meter = s.query(func.count(CONSU.Meter_Number)).filter(CONSU.Meter_Number==5).all()
-
-#How many files have we received?
-files_received = s.query(func.count(HEADR.id)).all()
-
-#What was the last file to be received
-last_file_uploaded = s.query(HEADR).order_by( desc(HEADR.Upload_Datetime)).first()
-last_file_uploaded.__dict__['File_Generation_Number']
-
-
+r = s.query(HEADR).get(1)
+print(r.__dict__)
 #closing session
 s.close()
